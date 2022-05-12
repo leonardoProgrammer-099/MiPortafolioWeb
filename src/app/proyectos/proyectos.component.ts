21bced740d6db3937b33913ea728f63adccca2b0
Leonardo Garcia <darkargarcia85@gmail.com>
2022-05-12 11:10:31 -0500

    Arrelgo de imagenes , se arreglo el bug no mostraba imagen

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -11,7 +11,7 @@ export class ProyectosComponent implements OnInit {
 
   proyects : any[] = [
     {
-      img : '../../assets/img/ptoyects/hnos transmedinas/1.png',
+      img : 'https://firebasestorage.googleapis.com/v0/b/leonardo-portafolio.appspot.com/o/hnos.png?alt=media&token=8314456d-7d55-4079-856d-7d79a5de68b0',
       text: 'Programa para el monitoreo de viajes de lazaro cardenas',
       btnGit: true,
       btnDemo: false,
@@ -19,7 +19,7 @@ export class ProyectosComponent implements OnInit {
       urlDemo:''
     },
     {
-      img : '../../assets/img/ptoyects/detector de rostro/1.jpg',
+      img : 'https://firebasestorage.googleapis.com/v0/b/leonardo-portafolio.appspot.com/o/1.jpg?alt=media&token=bc06b690-e9a4-4342-a841-4ffd111d9fac',
       text: 'Deteccion de rostros y detector de expresiones',
       btnGit: true,
       btnDemo: true,
@@ -27,7 +27,7 @@ export class ProyectosComponent implements OnInit {
       urlDemo:'https://detectar-rostro-3ee10.web.app/'
     },
     {
-      img : '../../assets/img/ptoyects/diseñoOTC/1.png',
+      img : 'https://firebasestorage.googleapis.com/v0/b/leonardo-portafolio.appspot.com/o/disen%CC%83o.png?alt=media&token=f8dc9568-98c5-43bc-932e-0634ef1ea746',
       text: 'Diseño para pagina web de compra y venta de oro',
       btnGit: true,
       btnDemo: false,
@@ -35,7 +35,7 @@ export class ProyectosComponent implements OnInit {
       urlDemo:''
     },
     {
-      img : '../../assets/img/ptoyects/coffeestore/1.png',
+      img : 'https://firebasestorage.googleapis.com/v0/b/leonardo-portafolio.appspot.com/o/coffee.png?alt=media&token=a95cf3bd-1f81-4b81-9c6d-0f0667f378aa',
       text: 'Tienda Online para comprar cafe',
       btnGit: true,
       btnDemo: false,
@@ -43,7 +43,7 @@ export class ProyectosComponent implements OnInit {
       urlDemo:''
     },
     {
-      img : '../../assets/img/ptoyects/tiendafarmacia/1.png',
+      img : 'https://firebasestorage.googleapis.com/v0/b/leonardo-portafolio.appspot.com/o/famacia.png?alt=media&token=429686d3-aadc-400c-ae03-52c9b148720e',
       text: 'Pagina web de farmacias san juan',
       btnGit: true,
       btnDemo: false,
@@ -51,7 +51,7 @@ export class ProyectosComponent implements OnInit {
       urlDemo:''
     },
     {
-      img : '../../assets/img/ptoyects/dipeli/1.png',
+      img : 'https://firebasestorage.googleapis.com/v0/b/leonardo-portafolio.appspot.com/o/1.png?alt=media&token=613e6446-7006-4c38-9d20-fe492c8b5a9a',
       text: 'Software para la gestion de compra , venta , registro e inventario de peliculas',
       btnGit: false,
       btnDemo: false,
